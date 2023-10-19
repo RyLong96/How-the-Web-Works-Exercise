e1a21587c6ba5245d2c9b6aa78204e7b1b323160
RyLong96 <rylong96@gmail.com>
2023-10-18 21:57:57 -0400

    Initial Commit

---
+++ app.js
@@ -0,0 +1,37 @@
+//what is HTTP?
+//The type of protocols used for browser and server to talk.
+
+//What is URL?
+//The entirety of protocol, hostname, port, resource, and query.
+
+//What is DNS?
+//Domain Name System, it is the nickname for ther server
+
+//What is a query string
+//what will be used to get the correct page
+
+//What is an HTTP request
+//getting information
+
+//What is an HTTP response
+//the what the page is requesting from the server
+
+//What is an HTTP header? Give a couple examples of request and response headers you have seen.
+//Key value pairs that give more info about the request or the response.
+
+//accept-language
+//accept-datetime
+
+//what are some process that happen when you type "http://somesite.com/some/page.html"
+//browser makes a request to the IP address
+//server sends back a response
+//browsers makes a  dom from the HTML and requests all other resources needed.
+
+//Part Two Practice Tools
+//1. Using curl, make a GET request to the icanhazdadjoke.com API to fond all jokes involving the word "pirate".
+// in cmd line - 
+
+//2. Use dig to find what the IP address is for icanhazdadjoke.com
+//dig icanhazdadjoke.com
+//104.27.157.79
+
